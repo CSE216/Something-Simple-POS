a4fc962560051fd9897107550bb9ac0e94e098d7
Jess <Jess@192.168.2.2>
2015-09-24 21:03:25 -0400

    Wrote code

--- SomethingSimplePOS/src/somethingsimplepos/Register.java
+++ SomethingSimplePOS/src/somethingsimplepos/Register.java
@@ -7,15 +7,66 @@ package somethingsimplepos;
  */
 /**
  *
- * @author Pablo
+ * @author Jess
  */
 public class Register {
-    public int userId;
-    public ProductCatalog catalog;
-
-    public Register(int userId) {
+    private int userId;
+    private int regId;
+    private ProductCatalog catalog; 
+    private Sale currentSale;
+    
+    public Register(int userId, int regId) {
         catalog = ProductCatalog.getInstance();
         this.userId = userId;
+        this.regId = regId;
     }
     
+    public void makeNewSale(){
+        if((currentSale!=null) || (currentSale.isComplete() == true)){
+        currentSale = new Sale();
+        }
+    }
+    
+    public void enterItem(int itemId, int quantity){
+        currentSale.makeLineItem(itemId, quantity);
+    }
+    
+    public double makePayment(double amountTendered, String paymentType){
+        currentSale.makePayment(amountTendered);
+        return amountTendered - getTotalTax();
+    }
+    
+    public void endSale(){
+        currentSale.setComplete(); //Sale method for end of sale needed
+        //inform database class that sale has ended?
+    }
+    
+    public double getTotal(){
+        double total = currentSale.getTotal();
+        return total;
+    }
+    
+    public double getTotalTax(){
+        return currentSale.getTotal()*1.06;
+    }
+    
+    public void cancelItem(int itemId){
+        currentSale.removeItem(itemId);
+    }
+    
+    public String printReceipt(){
+        return "";
+    }
+    
+    public void suspendSale(){
+        //suspend current sale
+    }
+    
+    public void resumeSale(){
+        //resume current sale
+    }
+    
+    public void managerOverride(int managerId){
+        //handle manager override 
+    }
 }
